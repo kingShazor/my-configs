16d20662aabc480325c8edd8c0d825fa6f277acc
Philipp Meister <philipp.meister@hacon.de>
2025-05-26 08:15:16 +0200

    config gespeichert

--- cfgs/config.fish
+++ cfgs/config.fish
@@ -1,9 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
+    set -x PATH $PATH /opt/hacon/dokugen/bin
+    set -x RELBUILDROOT /opt/hacon/dokugen
+    set -x BUILDROOT /opt/hacon/dokugen
+    set -x DISPLAY ""
+    if not contains $HOME/.local/bin $PATH
+        set -Ux PATH $PATH $HOME/.local/bin
+    end
+
+    # Sourcen der Setup-Datei
+    source /home/pme/.config/tps_dev_setup/profile
 end
 alias ls='ls -la'
-#function fish_prompt
-#    printf '%s%s%s > '
-#        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)   ...
-#end
