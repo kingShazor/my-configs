acbcf27d2ec131c8ab0788a10a78dd268ced14b8
Philipp Meister <philipp.meister@hacon.de>
2025-07-28 19:22:23 +0200

    sichern von meinen working dir

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,18 @@
 if status is-interactive
-    zoxide init fish | source
     # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    set -x PATH $PATH /opt/hacon/dokugen/bin
+    set -x RELBUILDROOT /opt/hacon/dokugen
+    set -x BUILDROOT /opt/hacon/dokugen
+    set -xg EDITOR nvim
+    set -x PATH $HOME/bin $PATH
+    set -x PAGER less.sh
+    #set -x DISPLAY ""
+    if not contains $HOME/.local/bin $PATH
+        set -Ux PATH $PATH $HOME/.local/bin
+    end
+
+    # Sourcen der Setup-Datei
+    source /home/pme/.config/tps_dev_setup/profile
 end
+alias ls='ls -la'
