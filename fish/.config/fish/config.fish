43c8745eb6203c9e0622c0490a258c860121ae54
kingShazor <175230427+kingShazor@users.noreply.github.com>
2026-01-24 13:40:04 +0100

    atuin setup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
+    atuin init fish | source
     # PATH-Einträge nur hinzufügen, wenn sie fehlen
-    for dir in $HOME/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.atuin/bin /usr/local/go/bin /snap/bin /opt/hacon/dokugen/bin /usr/bin
+    for dir in $HOME/bin $HOME/.cargo/bin $HOME/.local/bin /usr/local/go/bin /snap/bin /opt/hacon/dokugen/bin /usr/bin
         if test -d $dir
             if not contains $dir $PATH
                 set -gx PATH $dir $PATH
