d29c56129e724e645cec7b07a2ea75fcc031f8ba
kingShazor <175230427+kingShazor@users.noreply.github.com>
2026-01-25 11:58:15 +0100

    i3-wrapper

---
+++ tools/i3status-wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+HOST=$(hostname)
+
+if [ "$HOST" = "shazor-Ubuntu" ]; then
+    exec i3status-rs ~/.config/i3status-rust/config_ubuntu_gaming.toml
+else
+    exec i3status-rs ~/.config/i3status-rust/config.toml
+    # --profile laptop
+fi
